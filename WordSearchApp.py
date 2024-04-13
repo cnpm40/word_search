61b076cf958074f88abccec7c2013e64a1d8f194
huynt119 <22022509@vnu.edu.vn>
2024-04-13 20:44:34 +0700

    Đổi thư viện customtkinker --> tkinker

--- WordSearchApp.py
+++ WordSearchApp.py
@@ -1,6 +1,7 @@
-import customtkinter as ctk
+import tkinter as tk
+import word_search_generator
 
-class WordSearchApp(ctk.CTk):
+class WordSearchApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
@@ -8,37 +9,38 @@ class WordSearchApp(ctk.CTk):
         self.iconbitmap("./image/w.ico")
         self.geometry("1280x720")
 
-        game_header = ctk.CTkFrame(master= self)
-        game_header.pack(fill=ctk.X, side=ctk.TOP)
-        heading = ctk.CTkLabel(game_header,
+        game_header = tk.Frame(master= self)
+        game_header.pack(fill=tk.X, side=tk.TOP)
+        heading = tk.Label(game_header,
                        text='Word Search Game',
                        font=('Helvetica', 23, 'bold'),
-                       fg_color='transparent')
-        heading.pack(expand=True, fill=ctk.X, pady=12)
+                       fg='blue')
+        heading.pack(expand=True, fill=tk.X, pady=12)
 
-        self.write_button = ctk.CTkButton(self, text= "Write your own words", command= self.type_word)
+        self.write_button = tk.Button(self, text= "Write your own words", command= self.make_entry_window)
         self.write_button.pack(pady= 200)
 
-        self.gen_button = ctk.CTkButton(self, text= "Generate Word")
+        self.gen_button = tk.Button(self, text= "Generate Word")
         self.gen_button.pack(pady= 100)
     
-    def type_word(self):
-        dialog = ctk.CTkInputDialog(text="Type your own words, split by ',' :", title="Type words")
-        data = dialog.get_input()
-        if data:
-            self.words = data.split(",")
-            self.words = [word.strip() for word in self.words]
-            self.print_word()
-        else:
-            print("")
+    def make_entry_window(self):
+        entry_window = tk.Toplevel(self)
+        entry_window.geometry('600x350')
+        entry_window.iconbitmap("./image/w.ico")
+
+        text_label = tk.Label(entry_window, text= 'Type your words here, split by ",":', font=("", 12))
+        text_label.pack(pady= 10, ipady= 10)
+
+        entry = tk.Text(entry_window, width= 70, height= 10, borderwidth= 10)
+        entry.pack(pady= 10)
+        
+
 
     def print_word(self):
         print(self.words)
+
         
 def main():
-    ctk.set_appearance_mode("light")
-    ctk.set_default_color_theme("dark-blue")
-
     app = WordSearchApp()
     app.mainloop()
 
